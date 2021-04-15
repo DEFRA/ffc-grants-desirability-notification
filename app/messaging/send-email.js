691a26f22a123a02f994aefc46e963752ab9bf7f
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-04-15 17:22:06 +0100

    Send email

--- app/messaging/send-email.js
+++ app/messaging/send-email.js
@@ -1,8 +1,28 @@
+const notifyConfig = require('../config/notify')
+const NotifyClient = require('notifications-node-client').NotifyClient
+
+const notifyClient = new NotifyClient(notifyConfig.notifyApiKey)
+
 module.exports = async function (msg, submissionReceiver) {
   try {
-    const { body } = msg
+    const { body, correlationId } = msg
+
     console.log('Received message:')
     console.log(body)
+    console.log(`CorrelationId: ${correlationId}`)
+
+    const templateId = body.applicantEmail.notifyTemplate
+    const emailAddress = body.applicantEmail.emailAddress
+    const personalisation = body.applicantEmail.details
+
+    notifyClient
+      .sendEmail(templateId, emailAddress, {
+        personalisation,
+        reference: personalisation.referenceNumber
+      })
+      .then(response => console.log('SUCCESS SENDING EMAIL'))
+      .catch(err => console.error('FAILED SENDING EMAIL'))
+
     await submissionReceiver.completeMessage(msg)
   } catch (err) {
     console.error('Unable to process message')
