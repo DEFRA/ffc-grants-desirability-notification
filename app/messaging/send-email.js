aa391b96af8ef94cd5a1fbef52a6ae16dd2e77e1
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-04-28 06:47:31 +0100

    Tidy up

--- app/messaging/send-email.js
+++ app/messaging/send-email.js
@@ -5,31 +5,22 @@ const notifyClient = new NotifyClient(notifyConfig.notifyApiKey)
 
 module.exports = async function (msg, submissionReceiver) {
   try {
-    const { body, correlationId } = msg
-
-    console.log('Received message:')
-    console.log(body.applicantEmail)
-    console.log(`CorrelationId: ${correlationId}`)
-
+    const { body } = msg
     const templateId = body.applicantEmail.notifyTemplate
     const emailAddress = body.applicantEmail.emailAddress
     const personalisation = body.applicantEmail.details
 
-    try {
-      await notifyClient.sendEmail(templateId, emailAddress, {
-        personalisation,
-        reference: personalisation.referenceNumber
-      })
-      console.log('SUCCESS SENDING EMAIL')
-    } catch (err) {
-      console.log('FAILED SENDING EMAIL')
-      console.log(JSON.stringify(err, null, 2))
-    }
+    await notifyClient.sendEmail(templateId, emailAddress, {
+      personalisation,
+      reference: personalisation.referenceNumber
+    })
 
+    console.log('SUCCESS SENDING EMAIL')
     await submissionReceiver.completeMessage(msg)
   } catch (err) {
-    console.error('Unable to process message')
-    console.error(err)
+    console.log('FAILED SENDING EMAIL')
+    console.log(JSON.stringify(err, null, 2))
+    console.error('Abandoning message')
     await submissionReceiver.abandonMessage(msg)
   }
 }
