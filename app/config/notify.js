880ba8ae76ef27c199213c2d3635a8d28b041732
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-04-28 06:52:33 +0100

    Validate config

--- app/config/notify.js
+++ app/config/notify.js
@@ -1,7 +1,31 @@
-module.exports = {
-  notifyApiKey: process.env.NOTIFY_API_KEY,  
+const Joi = require('joi')
+
+// Define config schema
+const schema = Joi.object({
+  notifyApiKey: Joi.string().required(),
+  appInsights: Joi.object({
+    key: Joi.string(),
+    role: Joi.string()
+  })
+})
+
+// Build config
+const config = {
+  notifyApiKey: process.env.NOTIFY_API_KEY,
   appInsights: {
     key: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
     role: process.env.APPINSIGHTS_CLOUDROLE
   }
 }
+
+// Validate config
+const result = schema.validate(config, {
+  abortEarly: false
+})
+
+// Throw if config is invalid
+if (result.error) {
+  throw new Error(`The sharepoint config is invalid. ${result.error.message}`)
+}
+
+module.exports = result.value
