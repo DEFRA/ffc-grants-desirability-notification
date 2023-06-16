0bc9196d2c2c50df05f9fee9218cd931ceaa51cf
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-06-16 01:44:25 +0100

    replace tests

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,24 +1,43 @@
+// Mocks
 jest.mock('../../app/server')
 jest.mock('../../app/messaging/receivers')
-jest.mock('../../app/messaging/send-email')
-const { setup } = require('../../app/services/app-insights')
+jest.mock('../../app/messaging/send-email', () => "test")
 jest.mock('../../app/services/app-insights')
-const server = require('../../app/server')
-server.start = jest.fn(async () => { })
+
 const receivers = require('../../app/messaging/receivers')
+const mockStartSubmissionReceiver = jest.fn((d) => null)
+receivers.startSubmissionReceiver = mockStartSubmissionReceiver
 
-receivers.startSubmissionReceiver = jest.fn((a) => {})
-const indexInit = require('../../app/index')
-
-afterEach(() => {
-  jest.clearAllMocks()
+// Spies
+const server = require('../../app/server')
+const mockStartSpy = jest.spyOn(server, 'start').mockImplementation(async () => {
+  console.log('Mock: Server running on %s', server.info.uri)
 })
-describe('get indexInit setup defined', () => {
-  test('Should be defined', () => {
-    expect(indexInit).toBeDefined()
+
+const mockAppInsightsSpy = jest.spyOn(require('../../app/services/app-insights'), 'setup').mockImplementation(() => { return null })
+const logSpy = jest.spyOn(console, 'log').mockImplementation(() => { return null })
+const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { return null })
+
+describe('Index', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
   })
-  test('Should call setup once', () => {
-    expect(require('../../app/index')).toEqual({})
-    expect(setup).toHaveBeenCalledTimes(0)
+
+  it('Should call server.start', async () => {
+    expect.assertions(6)
+
+    require('../../app/index')
+    expect(mockStartSubmissionReceiver).toHaveBeenCalledTimes(1)
+    expect(mockStartSubmissionReceiver).toHaveBeenCalledWith('test')
+    expect(mockAppInsightsSpy).toHaveBeenCalledTimes(1)
+    expect(mockAppInsightsSpy).toHaveBeenCalledWith()
+    expect(mockStartSpy).toHaveBeenCalledTimes(1)
+    expect(logSpy).toHaveBeenCalledWith('Mock: Server running on %s', server.info.uri)
+  })
+
+  it('Should exit on unhandledRejection', async () => {
+    expect.assertions(1)
+    process.emit('unhandledRejection')
+    expect(mockExit).toHaveBeenCalledWith(1)
   })
 })
